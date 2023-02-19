44a8826e0524a0d9a07694a41c04178898ff5586
SamChus <samuelchigo55@gmail.com>
2023-02-19 11:37:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Board from "./Components/Board";
+import Nav from "./Components/Navbar";
+import Profile from "./Components/Profile";
+import GlobalStyle from "./Components/styles/Global";
+import styled from "styled-components";
+import { data } from "./services/data";
+
+
+const Container = styled.div`
+  padding: 24px;
+
+
+
+  @media screen and (min-width: 768px){
+    display: flex;
+    justify-content: center;
+  }
+
+`
+
 
 function App() {
+  const [page, setPage] = useState('home')
+const [interns, setInterns] = useState(data)
+// const [rewarded, setRewarded] = useState([])
+
+
+
+  const handleReward = (intern) => {
+    const newInterns = interns.filter((m) => m.id !== intern.id);
+    setInterns(newInterns)
+  
+  };
+
+  const handlePage = (intern) =>{
+    setPage('profile')
+    console.log(intern);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+      <Nav setPage={setPage}/>
+      <Container>
+      {page === 'home' ? <Board interns={interns} onReward={handleReward} onPage={handlePage}/> : <Profile  name={'Boss Namo'}/>}
+
+      </Container>
     </div>
   );
 }
---
+++ src/services/data.js
@@ -0,0 +1,126 @@
+
+
+export const data = [
+    {
+        id: 1,
+        name: 'TIMOTHY	ACHONU',
+        image: "img/avatar.jpg",
+        track: "Frontend",
+        score: 2080
+    },
+    {
+        id: 2,
+        name: 'SOCHIMA	ONAH',
+        image: "img/avatar2.jpg",
+        track: "Frontend",
+        score: 2080
+    },
+    {
+        id: 4,
+        name: 'NKEMDILI	EZIKE',
+        image: "img/avatar4.jpg",
+        track: "Frontend",
+        score: 2080
+    },
+    {
+        id: 5,
+        name: 'CHARLES	ORIIH',
+        image: "img/avatar5.jpg",
+        track: "Frontend",
+        score: 2080
+    },
+    {
+        id: 6,
+        name: 'CHUKWUEBUKA EDWARD',
+        image: "img/avatar6.jpg",
+        track: "Frontend",
+        score: 2080
+    },
+    {
+        id: 7,
+        name: 'CHINEDU IBELACHI',
+        image: "img/avatar7.jpg",
+        track: "Frontend",
+        score: 2080
+    },
+    {
+        id: 8,
+        name: 'CHIMAOBI CHUKWUEGBO',
+        image: "img/avatar8.jpg",
+        track: "Frontend",
+        score: 2080
+    },
+    {
+        id: 9,
+        name: 'CHIOMA UGWUODO',
+        image: "img/avatar9.jpg",
+        track: "Frontend",
+        score: 2080
+    },
+    {
+        id: 10,
+        name: 'ONYINYECHI OKEKE',
+        image: "img/avatar10.jpg",
+        track: "Frontend",
+        score: 2080
+    },
+    {
+        id: 11,
+        name: 'CHINYERE	AKANNO',
+        image: "img/avatar11.jpg",
+        track: "Backend",
+        score: 2080
+    },
+    {
+        id: 12,
+        name: 'NANCY OKEKE',
+        image: "img/avatar2.jpg",
+        track: "Frontend",
+        score: 2080
+    },
+    {
+        id: 14,
+        name: 'IFEANYI ATTAH',
+        image: "img/avatar4.jpg",
+        track: "Backend",
+        score: 2080
+    },
+    {
+        id: 15,
+        name: 'JONATHAN	AMOBI',
+        image: "img/avatar5.jpg",
+        track: "Backend",
+        score: 2080
+    },
+    {
+        id: 16,
+        name: 'MOSES NJOKU',
+        image: "img/avatar6.jpg",
+        track: "Frontend",
+        score: 2080
+    },
+    {
+        id: 17,
+        name: 'PAUL	OLISAELOKA',
+        image: "img/avatar7.jpg",
+        track: "Frontend",
+        score: 2080
+    },
+    {
+        id: 18,
+        name: 'MICHAEL	ORJI',
+        image: "img/avatar8.jpg",
+        track: "Backend",
+        score: 2080
+    },
+    {
+        id: 19,
+        name: 'CHIOMA UGWUODO',
+        image: "img/avatar9.jpg",
+        track: "Backend",
+        score: 2080
+    },
+ 
+]
+
+		
